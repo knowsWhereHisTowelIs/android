f80c611bd058c6aea7dac561b72e5f8622f7d2a8
knowsWhereHisTowelIs <cscalebslater@gmail.com>
2014-12-30 22:24:58 -0500

    Android part
    asdf

---
+++ gen/com/color/chooser/game/android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.color.chooser.game.android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/color/chooser/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.color.chooser.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.color.chooser.game.ColorChooserGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ColorChooserGame(), config);
+	}
+}
